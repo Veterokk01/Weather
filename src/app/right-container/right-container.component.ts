5219f8108e1fecef162eb71f6e558f761bad7011
Vetrok001 <Kirill.ss208@gmail.com>
2025-09-19 21:18:24 +0300

    Доработали правую сторону сайта

--- src/app/right-container/right-container.component.ts
+++ src/app/right-container/right-container.component.ts
@@ -7,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrl: './right-container.component.css'
 })
 export class RightContainerComponent {
+  today:boolean = false;
+  week:boolean = true;
 
+  celsius:boolean = true;
+  farenheit:boolean = false;
+
+  onTodayClick() {
+    this.today = true;
+    this.week = false;
+  }
+
+  onWeekClick() {
+    this.today = false;
+    this.week = true;
+  }
+
+  onCelsiusClick() {
+    this.celsius = true;
+    this.farenheit = false;
+  }
+
+  onFarenheitClick() {
+    this.celsius = false;
+    this.farenheit = true;
+  }
 }
