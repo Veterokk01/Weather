40634cbbef590e66bd174c8eb8720d0217a70c52
Vetrok001 <Kirill.ss208@gmail.com>
2025-09-24 21:14:43 +0300

    Почти закончили правые карточки

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-icons';
-
+import { FaIconComponent } from '@fortawesome/angular-fontawesome'
+import { faMagnifyingGlass, faLocationDot, faCloud, faCloudRain } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-left-container',
@@ -9,7 +8,11 @@ import { faMagnifyingGlass, faLocationDot } from '@fortawesome/free-solid-svg-ic
   templateUrl: './left-container.component.html',
   styleUrl: './left-container.component.css'
 })
+
 export class LeftContainerComponent {
-  faMagnifyingGlass:any = faMagnifyingGlass
-  faLocationDot:any = faLocationDot
+  faMagnifyingGlass:any = faMagnifyingGlass;
+  faLocationDot:any = faLocationDot;
+
+  faCloud:any = faCloud;
+  faCloudRain:any = faCloudRain;
 }
